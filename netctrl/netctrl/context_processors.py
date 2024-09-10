5bcd02938bddf7f6c860cf18eac5ee835bfd1bb4
Bob King <bob@getready1.com>
2024-09-10 12:07:10 -0700

    add NC_GRIDRULER, context processor and view logic

--- netctrl/netctrl/context_processors.py
+++ netctrl/netctrl/context_processors.py
@@ -1,4 +1,5 @@
-from django.conf import settings
+from django.conf import settings as app_settings
 
 def settings(request):
-    return {"settings": settings}
+    return {"settings": app_settings,
+            }
