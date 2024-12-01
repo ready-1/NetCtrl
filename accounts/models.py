ef99e7086d64f027ad900cc2bd4b9f90fabd7626
Bob King <bob@getready1.com>
2024-11-30 16:39:28 -0800

    add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    display_name = models.CharField(max_length=50, blank=True, null=True)
+    notify_on_approval = models.BooleanField(default=False)
