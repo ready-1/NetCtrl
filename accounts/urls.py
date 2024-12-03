168fb731229334a25ae1504396c04ab7f8556d8e
Bob King <bob@getready1.com>
2024-12-02 16:02:36 -0800

    add urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('pending-approvals/', views.pending_approvals, name='pending_approvals'),
+    path('approve-user/<int:user_id>/', views.approve_user, name='approve_user'),
+]
