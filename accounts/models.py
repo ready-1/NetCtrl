98c64929fe71dc3234a89fbf3ebd39f269ea8292
Bob King <bob@getready1.com>
2024-12-03 20:10:36 -0800

    add UserManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.conf import settings
 
 class CustomUser(AbstractUser):
     notify_on_approval = models.BooleanField(default=False)
+    objects = UserManager()
 
     def __str__(self):
         return self.username
