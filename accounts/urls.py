f9765fd863a183e865e0bfa8c5e72c72db2f0d90
Bob King <bob@getready1.com>
2024-12-03 20:30:55 -0800

    add logout and change order

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('pending-approvals/', pending_approvals, name='pending_approvals'),
     path('approve-user/<int:user_id>/', approve_user, name='approve_user'),
-    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    
 ]
