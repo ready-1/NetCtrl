e91fd4831ea734deee54d5de881f0b7a0e5a3ef9
Bob King <bob@getready1.com>
2024-12-03 20:10:52 -0800

    add LoginView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import pending_approvals, approve_user, SignUpView
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('pending-approvals/', pending_approvals, name='pending_approvals'),
     path('approve-user/<int:user_id>/', approve_user, name='approve_user'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
