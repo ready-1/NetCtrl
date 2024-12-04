c491620045017b8ab96e936eb899d33c9d1257ab
Bob King <bob@getready1.com>
2024-12-03 20:10:15 -0800

    change User to CustomUser

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,26 @@
-from django import forms
+# from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from accounts.models import CustomUser
 
-User = get_user_model()
+
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = ('username', 'email', 'first_name', 'last_name')
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+    def clean_username(self):
+        print(f"Using model in clean_username: {self._meta.model}")
+        return super().clean_username()
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('username', 'email', 'first_name', 'last_name', 'notify_on_approval')
