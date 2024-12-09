a801ff44b8e48988e59419afce284d9dcf144e31
Bob King <bob@getready1.com>
2024-12-08 19:59:30 -0800

    add f&l name caps, display name prop

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,23 @@ class CustomUser(AbstractUser):
     notify_on_approval = models.BooleanField(default=False)
     objects = UserManager()
 
+    def save(self, *args, **kwargs):
+        # Capitalize first and last names
+        if self.first_name:
+            self.first_name = self.first_name.capitalize()
+        if self.last_name:
+            self.last_name = self.last_name.capitalize()
+        super().save(*args, **kwargs)
+
+    @property
+    def display_name(self):
+        # Generate display name as "First L." or just "First" if last_name is missing
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name[0].upper()}."
+        elif self.first_name:
+            return self.first_name
+        return self.username  # Fallback to username if names are missing
+
     def __str__(self):
         return self.username
 
