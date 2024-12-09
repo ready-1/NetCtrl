418e365abd876d424b54e08268c168438edef4d1
Bob King <bob@getready1.com>
2024-12-09 11:23:51 -0800

    add switch model

--- net_core/admin.py
+++ net_core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Switch
 
-# Register your models here.
+@admin.register(Switch)
+class SwitchAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('name', 'ip_address_in_band', 'status', 'model', 'location', 'last_sync')
+    
+    # Fields to enable search functionality
+    search_fields = ('name', 'ip_address_in_band', 'mac_address', 'model', 'serial_number', 'location')
+    
+    # Fields to filter results in the admin interface
+    list_filter = ('status', 'model', 'location')
+    
+    # Default ordering of the list view
+    ordering = ('name',)
