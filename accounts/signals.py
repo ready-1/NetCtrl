f8598488d4ba318363d2c4eaafc51f4a9d874bd0
Bob King <bob@getready1.com>
2024-12-08 19:59:50 -0800

    update pending user message

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,19 +1,20 @@
-from django.db.models.signals import post_save
+from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-from django.core.mail import send_mail
+from django.contrib import messages
+from django.urls import reverse
 from .models import CustomUser
-from django.conf import settings
 
-@receiver(post_save, sender=CustomUser)
-def notify_approvers(sender, instance, created, **kwargs):
-    if created and not instance.is_active:
-        approvers = CustomUser.objects.filter(notify_on_approval=True)
-        for approver in approvers:
-            # Send email notification
-            send_mail(
-                subject="New User Pending Approval",
-                message=f"A new user '{instance.username}' is awaiting approval.",
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[approver.email],
-                fail_silently=False,
+@receiver(user_logged_in)
+def admin_login_notification(sender, request, user, **kwargs):
+    # Check if the user is an admin and should be notified
+    if user.is_staff and user.notify_on_approval:
+        pending_users = CustomUser.objects.filter(is_active=False)
+        if pending_users.exists():
+            user_list = ', '.join([pending_user.username for pending_user in pending_users])
+            link = reverse('admin:accounts_customuser_changelist')
+            messages.info(
+                request,
+                f"The following users are pending approval: {', '.join([u.display_name for u in pending_users])}. "
+                f"<a href='{link}'>Review now</a>",
+                extra_tags='safe',
             )
