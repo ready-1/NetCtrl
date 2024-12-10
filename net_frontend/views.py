f47c350b4cdeeeabe2c3f5557461c65ac4821b20
Bob King <bob@getready1.com>
2024-12-09 20:35:48 -0800

    add permissions required for add/edit/delete with styled 403 page

--- net_frontend/views.py
+++ net_frontend/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from net_core.models import Switch
 from net_frontend.forms import SwitchForm
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 class SwitchListView(ListView):
     model = Switch
@@ -13,29 +14,32 @@ class SwitchDetailView(DetailView):
     template_name = "net_frontend/switch_detail.html"
     context_object_name = "switch"
 
-class SwitchCreateView(CreateView):
+class SwitchCreateView(PermissionRequiredMixin, CreateView):
     model = Switch
     form_class = SwitchForm
     template_name = "net_frontend/switch_form.html"
     success_url = reverse_lazy("net_frontend:switch_list")
+    permission_required = "net_core.add_switch"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['action'] = "Create"
         return context
 
-class SwitchUpdateView(UpdateView):
+class SwitchUpdateView(PermissionRequiredMixin,UpdateView):
     model = Switch
     form_class = SwitchForm
     template_name = "net_frontend/switch_form.html"
     success_url = reverse_lazy("net_frontend:switch_list")
+    permission_required = "net_core.change_switch"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['action'] = "Update"
         return context
 
-class SwitchDeleteView(DeleteView):
+class SwitchDeleteView(PermissionRequiredMixin,DeleteView):
     model = Switch
     template_name = "net_frontend/switch_confirm_delete.html"
     success_url = reverse_lazy("net_frontend:switch_list")
+    permission_required = "net_core.delete_switch"
