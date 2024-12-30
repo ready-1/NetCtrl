e94f4aae428beaf1cc9f51067409bde928eda340
Bob King <bob@getready1.com>
2024-12-30 08:13:03 -0800

    add set_bonjour_status()

--- foo.py
+++ foo.py
@@ -2,7 +2,7 @@ import os
 
 from pprint import pprint
 
-from net_core.api_helpers.device_helpers import get_device_name, set_device_name, reboot_device, config_copy, save_config, get_bonjour_status
+from net_core.api_helpers.device_helpers import get_device_name, set_device_name, reboot_device, config_copy, save_config, get_bonjour_status, set_bonjour_status
 from net_core.api_helpers.token_manager import TokenManager
 
 
@@ -23,8 +23,13 @@ def test_device_info():
         print(f"Successfully obtained token: {token}")
 
         
-        result = get_bonjour_status(SWITCH_IP, token)   
-        
+        result = set_bonjour_status(SWITCH_IP, token, "disabled")
+        print(result)
+        result = get_bonjour_status(SWITCH_IP, token)
+        print(result)
+        result = set_bonjour_status(SWITCH_IP, token, "enabled")
+        print(result)
+        result = get_bonjour_status(SWITCH_IP, token)
         print(result)
 
     except Exception as e:
--- net_core/api_helpers/device_helpers/__init__.py
+++ net_core/api_helpers/device_helpers/__init__.py
@@ -4,3 +4,4 @@ from .set_device_name import set_device_name
 from .reboot_device import reboot_device
 from .config_copy import config_copy, save_config
 from .get_bonjour_status import get_bonjour_status
+from .set_bonjour_status import set_bonjour_status
