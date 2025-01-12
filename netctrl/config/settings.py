6b7d322486868655bcd57136f951946792992f3a
Bob King <bob@getready1.com>
2025-01-12 15:51:31 -0500

    Add PostgreSQL database configuration and apply migrations

--- netctrl/config/settings.py
+++ netctrl/config/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netctrl',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
